6802b74a67616fd8c078b92fee091ac3762df41a
CallanSoftwareDev <c.jacka07@gmail.com>
2023-04-25 19:36:50 +0930

    Started development in ReactJS

--- src/script.js
+++
@@ -1,7 +0,0 @@
-function validate() {
-    if (document.getElementById('remember').checked) {
-        alert("checked");
-    } else {
-        alert("You didn't check it! Let me check it for you.");
-    }
-}
